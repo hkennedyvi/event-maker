64a59ccd180023a83ee1df5edc6a63e5f5ff0716
Sarah Vivoli <snvivoli@gmail.com>
2020-04-02 16:34:17 -0700

    current location loadmap

--- client/src/components/CurrentLocation/index.js
+++ client/src/components/CurrentLocation/index.js
@@ -52,6 +52,33 @@ export class CurrentLocation extends React.Component {
         }
         this.loadMap();
     }
+
+    loadMap() {
+        if (this.props && this.props.google) {
+            // checks if google is available
+            const { google } = this.props;
+            const maps = google.maps;
+
+            const mapRef = this.refs.map;
+
+            // reference to the actual DOM element
+            const node = ReactDOM.findDOMNode(mapRef);
+
+            let { zoom } = this.props;
+            const { lat, lng } = this.state.currentLocation;
+            const center = new maps.LatLng(lat, lng);
+            const mapConfig = Object.assign(
+                {},
+                {
+                    center: center,
+                    zoom: zoom
+                }
+            );
+
+            // maps.Map() is constructor that instantiates the map
+            this.map = new maps.Map(node, mapConfig);
+        }
+    }
 };
 
 export default CurrentLocation;
