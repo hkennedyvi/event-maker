6c819c4a695bfe3a9b7e5e8ddc7da74b9fa4f5b5
Sarah Vivoli <snvivoli@gmail.com>
2020-04-02 16:37:35 -0700

    deleted map component

--- client/src/components/CurrentLocation/index.js
+++ client/src/components/CurrentLocation/index.js
@@ -106,15 +106,15 @@ export class CurrentLocation extends React.Component {
 
     render() {
         const style = Object.assign({}, mapStyles.map);
-       return (
-         <div>
-           <div style={style} ref="map">
-             Loading map...
+        return (
+            <div>
+                <div style={style} ref="map">
+                    Loading map...
            </div>
-           {this.renderChildren()}
-         </div>
-       );
-     }
+                {this.renderChildren()}
+            </div>
+        );
+    }
 };
 
 export default CurrentLocation;
--- client/src/components/Map/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Map() {
-    return (
-        <div className="map"></div>
-    )
-}
-
-export default Map;
