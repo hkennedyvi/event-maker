32b28f7582b33bb80cbda8982b7dfc56f065bd59
Sarah Vivoli <snvivoli@gmail.com>
2020-04-02 16:35:12 -0700

    current location render

--- client/src/components/CurrentLocation/index.js
+++ client/src/components/CurrentLocation/index.js
@@ -94,6 +94,18 @@ export class CurrentLocation extends React.Component {
             });
         });
     }
+
+    render() {
+        const style = Object.assign({}, mapStyles.map);
+       return (
+         <div>
+           <div style={style} ref="map">
+             Loading map...
+           </div>
+           {this.renderChildren()}
+         </div>
+       );
+     }
 };
 
 export default CurrentLocation;
