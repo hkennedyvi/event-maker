1b58863fd99352257f01b84da278e40bfd3e4e76
Sarah Vivoli <snvivoli@gmail.com>
2020-04-02 16:29:24 -0700

    map container state

--- client/src/components/MapContainer/index.js
+++ client/src/components/MapContainer/index.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 export class MapContainer extends Component {
-
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {}
+      };
 };
 
 export default GoogleApiWrapper({
