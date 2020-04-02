84d137715eab408e41f9a37d0b0f3f6d6bd88e17
Sarah Vivoli <snvivoli@gmail.com>
2020-04-02 16:32:07 -0700

    current location state

--- client/src/components/CurrentLocation/index.js
+++ client/src/components/CurrentLocation/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export class CurrentLocation extends React.Component {
+    constructor(props) {
+        super(props);
 
+        const { lat, lng } = this.props.initialCenter;
+        this.state = {
+            currentLocation: {
+                lat: lat,
+                lng: lng
+            }
+        };
+    }
 };
 
 export default CurrentLocation;
