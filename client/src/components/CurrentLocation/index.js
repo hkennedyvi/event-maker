97856d3654aa1e4a9a9d046b272f76571f20c3ad
Sarah Vivoli <snvivoli@gmail.com>
2020-04-02 16:35:37 -0700

    current location default props

--- client/src/components/CurrentLocation/index.js
+++ client/src/components/CurrentLocation/index.js
@@ -109,3 +109,13 @@ export class CurrentLocation extends React.Component {
 };
 
 export default CurrentLocation;
+
+CurrentLocation.defaultProps = {
+    zoom: 14,
+    initialCenter: {
+        lat: -1.2884,
+        lng: 36.8233
+    },
+    centerAroundCurrentLocation: false,
+    visible: true
+};
