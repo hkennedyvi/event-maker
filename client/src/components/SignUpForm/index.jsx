95d179ffc26891758bf046a2090a5c92f99c1cdb
Howard Kennedy <hkennedyvi@gmail.com>
2020-04-02 19:22:46 -0700

    passport/bcrypt user auth started

--- client/src/components/SignUpForm/index.jsx
+++ client/src/components/SignUpForm/index.jsx
@@ -5,14 +5,14 @@ function SignUpForm(props) {
     return (
         <div>
 
-            <form onSubmit={(e) => {props.handleSubmit(e) }}>
+            <form onSubmit={(e) => {props.handleSubmit(e) }} action="POST">
                 <label for="email">EMAIL:</label><br />
                 <input type="email" id="email" name="email" placeholder="Email here" /><br />
                 <label for="lname">PASSWORD:</label><br />
                 <input type="password" id="password" name="password" placeholder="Password here" /><br /><br />
                 <input type="submit" value="Submit" />
             </form>
-            <FacebookBtn />
+            {/* <FacebookBtn /> */}
         </div>
     )
 }
