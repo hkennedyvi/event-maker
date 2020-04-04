2ed8233a4befdbead6ca4ed14e0157b97cf077f7
Sarah Vivoli <snvivoli@gmail.com>
2020-04-04 11:10:26 -0700

    eventscontroller findbyid

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -7,5 +7,11 @@ module.exports = {
         .sort({ date: -1 })
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
-    }
+    },
+    findByID: function(req, res) {
+        db
+        .findById(req.params.id)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
 };
