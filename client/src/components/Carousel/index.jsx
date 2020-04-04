6d1f176f7043d7895ee269b7f43f328b353e2f2e
programmerkim <jenniferkim.businessonly@gmail.com>
2020-04-04 16:28:24 -0700

    carousel page

--- client/src/components/Carousel/index.jsx
+++ client/src/components/Carousel/index.jsx
@@ -1,3 +1,14 @@
 import React from "react";
 import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
 "mdbreact";
+
+const CarouselPage = () => {
+  return (
+    <MDBContainer>
+      <MDBCarousel
+      activeItem={1}
+      length={3}
+      showControls={true}
+      showIndicators={true}
+      className="z-depth-1"
+    ></MDBCarousel>
