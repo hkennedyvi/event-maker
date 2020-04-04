6139b446f593bf57b78a2b8ecda0b7f6ee60e498
Sarah Vivoli <snvivoli@gmail.com>
2020-04-04 11:11:04 -0700

    eventscontroller create

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -14,4 +14,10 @@ module.exports = {
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
+    create: function(req, res) {
+        db
+        .create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    },
 };
