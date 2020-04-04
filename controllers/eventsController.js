c30f0f009c767db536564bc2e0185c5894512f2f
Sarah Vivoli <snvivoli@gmail.com>
2020-04-04 11:11:58 -0700

    eventscontroller remove

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -20,4 +20,11 @@ module.exports = {
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
+    remove: function(req, res) {
+        db
+        .findById({ _id: req.params.id })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
+    }
 };
