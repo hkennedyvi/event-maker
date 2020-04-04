1613218142b741bf05fd75f47f95c11473cef869
clauries <55369403+clauries@users.noreply.github.com>
2020-04-04 13:24:40 -0700

    saving progress before merging master in

--- client/src/components/SignUpForm/index.jsx
+++ client/src/components/SignUpForm/index.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
 import FacebookBtn from '../FacebookBtn';
+import { makeStyles } from '@material-ui/core/styles';
+import { TextField, FormHelperText, Card, CardActions, CardContent, Typography, Box } from '@material-ui/core';
+import './SignUpForm.css'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+            display: 'flex',
+            flexDirection: 'column',
+
+        },
+        
+    },
+
+    textfield: {
+        marginBottom: 15,
+    }
+
+
+    
+}));
 
 function SignUpForm(props) {
+    const classes = useStyles();
     return (
-        <div>
-
+        <div className={classes.root} >
+            <Box display="flex" >
+            <Card className={classes.cardRoot}>
+                <CardContent>
+                    <Typography variant="h5" component="h2">
+                        Sign In
+                    </Typography>
+                    <CardActions>
+            <form className={classes.formRoot}>
+                <TextField
+                    className={classes.textfield}
+                    required
+                    id="outlined-required"
+                    label="Email"
+                    variant="outlined"
+                />
+                <TextField
+                    className={classes.textfield}
+                    required
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    variant="outlined"
+                />
+            </form>
+            </CardActions>
+            </CardContent>
+            </Card>
+            </Box>
             <form onSubmit={(e) => { props.handleSubmit(e) }} action="POST">
                 <label for="email">EMAIL:</label><br />
                 <input type="email" id="email" name="email" placeholder="Email here" /><br />
