78e63124c085d472f09d78c97bd8853dc12dbfb3
Sarah Vivoli <snvivoli@gmail.com>
2020-04-07 13:35:08 -0700

    event dialog changed to form dialog

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,29 +3,36 @@ import MapContainer from "../components/MapContainer";
 import API from '../utils/API';
 
 function Home() {
-    const [newEvent, setNewEvent] = useState([]);
+    const [newEvent, setNewEvent] = useState({});
 
     function handleChange(event) {
         console.log(event.target.value);
+        let category = document.getElementById("category");
+        // console.log(category.value);
+        let location = document.getElementById("location");
+        let name = document.getElementById("name");
+        let notes = document.getElementById("notes");
+        setNewEvent({ 
+            category: category.value, 
+            name: name.value,
+            location: location.value,
+            notes: notes.value
+        });
     }
 
     function handlePost(event) {
         event.preventDefault();
         // console.log("Hi from post handler");
         // console.log(this);
-        let category = document.getElementById("category");
-        console.log(category.value);
-        let location = document.getElementById("location");
-        let name = document.getElementById("name");
-        let notes = document.getElementById("notes");
-        console.log(location.value);
+        console.log(newEvent);
         API.createEvent({ 
             category: "sports",
-            name: name.value,
-            location: location.value,
+            name: "name.value",
+            location: "location.value",
             participants: 6,
             duration: 2,
-            notes: notes.value
+            notes: "notes.value",
+            creator: "email"
         })
         .then(console.log("Event saved to database."))
         .catch(err => console.log(err));
