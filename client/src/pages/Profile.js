94b5b1c046afa45ce9def5d65d408864c437d8cb
Howard Kennedy <hkennedyvi@gmail.com>
2020-04-07 14:34:07 -0700

    events data reads on profile page, user param needs work

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,25 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import API from '../utils/API';
 
 function Profile() {
+
+    const [events, setEvents] = useState({});
+    const [user, setUser] = useState("hkenvi@yahoo.com");
+
+    useEffect(() => {
+        loadSavedEvents()
+    }, [])
+
+    function loadSavedEvents() {
+
+        API.getUserEvents(user).then(res => {
+            console.log(user)
+            console.log(res);
+
+            // setEvents(res.data);
+        })
+            .catch(err => console.log(err));
+    };
 
     return (
         <h1>MY PROFILE PAGE</h1>
