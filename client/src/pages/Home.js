e02c9c9306a0a19b125bcb9490a8cdbac5d29877
Howard Kennedy <hkennedyvi@gmail.com>
2020-04-09 15:08:18 -0700

    event location presets to current location

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -27,25 +27,20 @@ function Home() {
     function handleLocationGrab(currentLocation) {
         let eventLat = currentLocation.lat;
         let eventLng = currentLocation.lng;
-        console.log(currentLocation.lat);
-        console.log(currentLocation.lng);
-        // set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+
         Geocode.setApiKey("AIzaSyCRTz31ipS9i5nHfyWIs-mcSIQmWRxXTec");
 
-        // set response language. Defaults to english.
         Geocode.setLanguage("en");
 
-        // set response region. Its optional.
-        // A Geocoding request with region=es (Spain) will return the Spanish city.
         Geocode.setRegion("es");
 
-        // Enable or disable logs. Its optional.
         Geocode.enableDebug();
-        // {lat: 45.472815000000004, lng: -122.7741112}
+        
         Geocode.fromLatLng(eventLat, eventLng).then(
             response => {
                 const address = response.results[0].formatted_address;
                 console.log(address);
+                setUserLocation(address);
             },
             error => {
                 console.error(error);
