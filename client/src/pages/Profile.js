d8953d4c49e954aaa84584298032a85c0fa03276
clauries <55369403+clauries@users.noreply.github.com>
2020-04-09 18:52:57 -0700

    changed profile page layout

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import API from '../utils/API';
 import HistorySection from '../components/HistorySection';
+import UserInfo from '../components/UserInfo';
+
+const useStyles = makeStyles({
+    rootProfile: {
+        flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'row',
+        margin: '12% 2%',
+        // If we want the background to not be same height as events, uncomment align-items
+        // alignItems: 'flex-start',
+    },
+
+});
 
 function Profile() {
-
+    const classes = useStyles();
     const [madeEvents, setMadeEvents] = useState([]);
     const [attendedEvents, setAttendedEvents] = useState([]);
     const [user, setUser] = useState("hkenvi@yahoo.com");
@@ -27,7 +41,8 @@ function Profile() {
     };
 
     return (
-        <div>
+        <div className={classes.rootProfile}>
+            <UserInfo />
             <HistorySection madeEvents={ madeEvents } attendedEvents={ attendedEvents } />
         </div>
     )
