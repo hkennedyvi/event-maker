e2aae8f1edfdd192837d3d9e43b3a3d951438369
Howard Kennedy <hkennedyvi@gmail.com>
2020-04-13 14:59:02 -0700

    capitlization fixed on signup page

--- client/src/pages/SIgnup.js
+++ client/src/pages/SIgnup.js
@@ -18,7 +18,7 @@ function Signup() {
         setUser(userData);
        
         if (userData.password !== userData.confirmPassword) {
-            alert("passwords don't match");
+            alert("Passwords don't match");
         } else {
            
             axios
