42f5418734b929f4beec8cb11ff325f1ad215736
clauries <55369403+clauries@users.noreply.github.com>
2020-04-14 16:19:15 -0700

    Aligned buttons

--- client/src/components/JoinConfirm/index.jsx
+++ client/src/components/JoinConfirm/index.jsx
@@ -16,7 +16,8 @@ const useStyles = makeStyles({
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
         color: 'rgba(46, 34, 68, 1)',
         height: 48,
-        padding: '0 30px',
+        // padding: '0 30px',
+        
     }
 });
 
